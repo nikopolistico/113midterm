0135b50f8d6875f406eb88391cffc78558b2126d
Niko Polistico <166996904+nikopolistico@users.noreply.github.com>
2025-10-06 16:55:10 +0800

    Change router history mode to hash

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import LoginPage from '@/views/LoginPage.vue'
 import RegisterPage from '@/views/RegisterPage.vue'
 import LandingPage from '@/views/LandingPage.vue'
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
@@ -19,7 +19,7 @@ const router = createRouter({
       path: '/landing',
       name: 'landing',
       component: LandingPage,
-    }
+    },
   ],
 })
 
